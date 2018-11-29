4eb24131adfce511f8f25b63d955445d93c12f4a
Yuriy Kashnikov <yuriy@blockapps.net>
2018-11-29 14:38:23 +0700

    Make intToBytes32 convert number passed as string

--- test/util.test.js
+++ test/util.test.js
@@ -26,12 +26,46 @@ function positiveBytes(b,w) {
 }
 
 describe('util tests', function() {
-  it('converts negative numbers to and from hex', function() {
-    for(var k=0;k < 10000; k++) {
-      const n = Math.floor((Math.random() * MAX) - MAX/2);
-      const b = util.intToBytes32(n);
-      const m = bytes32ToInt(b);
-      m.should.equal(n);
-    }
+  describe('intToBytes32', function() {
+    // bytes32(9) as a hex string
+    const expectedResultFor9 = '0000000000000000000000000000000000000000000000000000000000000009';
+    // bytes32(17) as a hex string
+    const expectedResultFor17 = '0000000000000000000000000000000000000000000000000000000000000011';
+
+
+    it('converts negative numbers to and from hex', function() {
+      for(var k=0;k < 10000; k++) {
+        const n = Math.floor((Math.random() * MAX) - MAX/2);
+        const b = util.intToBytes32(n);
+        const m = bytes32ToInt(b);
+        m.should.equal(n);
+      }
+    });
+
+    it('Converting number', function() {
+      const resultFor9 = util.intToBytes32(9);
+      resultFor9.should.equal(expectedResultFor9, 'converting 9');
+      const resultFor17 = util.intToBytes32(17);
+      resultFor17.should.equal(expectedResultFor17, 'converting 17');
+    });
+
+    it('Converting number -- fails if non-integer', function() {
+      (() => util.intToBytes32(17.1)).should.throw(Error);
+    });
+
+    it('Converting string', function() {
+      const resultFor9 = util.intToBytes32('9');
+      resultFor9.should.equal(expectedResultFor9, 'converting 9');
+      const resultFo17 = util.intToBytes32('17');
+      resultFo17.should.equal(expectedResultFor17, 'converting 17');
+    });
+
+    it('Converting string -- fails if not a number', function() {
+      (() => util.intToBytes32('not a number')).should.throw(Error);
+    });
+
+    it('Converting string -- fails if non-integer', function() {
+      (() => util.intToBytes32('17.1')).should.throw(Error);
+    });
   });
 });
