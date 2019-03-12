dcc3710b5d8c6209f25186be4aa309ce76805c8d
Lior Saar <liorsaar1@gmail.com>
2019-03-12 11:24:30 -0700

    folders deleted

--- test/fixtures/SimpleStorage.sol
+++
@@ -1,9 +0,0 @@
-contract SimpleStorage {
-    uint storedData = 123;
-    function set(uint x) {
-        storedData = x;
-    }
-    function get() returns (uint) {
-        return storedData;
-    }
-}
--- test/fixtures/second/first/PQR.sol
+++
@@ -1,7 +0,0 @@
-import "../../first/first/ABC.sol";
-
-contract PQR is ABC {
-  uint constant P = 15;
-  uint constant Q = 16;
-  uint constant R = 17;
-}
